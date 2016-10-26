acb25de8137f5ea2944fc86f263d0173041a37ae
Maxklos <herzognikolaus@gmail.com>
2016-10-26 19:38:02 +0200

    Praxis Tewst
    Greating more bugs than we solved... Worth it!

--- LED Remote Control BETA/LED_Remote_Control_v2/LED_Remote_Control_v2.ino
+++ LED Remote Control BETA/LED_Remote_Control_v2/LED_Remote_Control_v2.ino
@@ -1,5 +1,5 @@
 //author: Benjamin Koderisch
-//version: 0.3.1 beta, 26.10.16
+//version: 0.3.2 beta, 26.10.16
 
 #include "RGBdriver.h"
 #include <IRremote.h>
@@ -12,43 +12,49 @@ decode_results results;
 #define DIO 3
 
 int btnPin = 7;
-RGBdriver Driver(CLK,DIO);
+RGBdriver Driver(CLK, DIO);
 int mFlag = 1; //flag for the current modus | bsp. 1, 2, 3...
-int sFlag = 0; //flag for the current status | bsp. 1 for off
+int sFlag = 1; //flag for the current status | bsp. 1 for off
 int btn;
-int vipFlag; // when the sFlag is 0 and a button is pressed it switches 
-             // to Flag 1 and go into the if loop 
+int vipFlag; // when the sFlag is 0 and a button is pressed it switches
+// to Flag 1 and go into the if loop
 
-void setup(){ 
+void setup() {
   pinMode(btnPin, INPUT);
   irrecv.enableIRIn();
-}  
+}
 
-void loop(){ 
+void loop() {
   Driver.begin();
   Serial.begin(9200);
-  switch(sFlag){
+  switch (sFlag) {
     case 0:
       case0();
       break;
     case 1:
       case1();
       break;
-    case 2:
-      case2();
-      break;
+      //case 2:
+      //case2();
+      //break;
   }
-}
+  delay(10);
+  Serial.println(sFlag);
+  Serial.print(".");
+  Serial.println(mFlag);
+  Serial.print("..");
+  Serial.println(btnPressed(results.value));
+  }
 
 
-void newMFlag(){
-  switch(mFlag){
+void newMFlag() {
+  switch (mFlag) {
     // off
     case 0:
-      setColor(0,0,0);
+      setColor(0, 0, 0);
       break;
     case 1:
-      setColor(255,255,255);
+      setColor(255, 255, 255);
       break;
     case 2:
       setColor(0, 255, 255);
@@ -74,140 +80,150 @@ void newMFlag(){
     case 9:
       setColor(0, 0, 0);
       break;
-    
+
   }
 }
 
 // sFlag = 0
-void case0();
-  setColor(0,0,0);
-  if(irrecv.decode(&results)){
-    sFlag = 1
-    btn = btnPressed(results.value);
-    vipFlag = 1;
+void case0()
+{
+  setColor(0, 0, 0);
+  if (irrecv.decode(&results))
+  {
+    int a = btnPressed(results.value);
+    if(a < 17 && a > 0){
+      sFlag = 1;
+      btn = btnPressed(results.value);   
+      vipFlag = 1;
+    }
+
   }
 
+}
+
 // sFlag = 1
-void case1(){
-  if(irrecv.decode(&results) || vipFlag == 1){
-    Serial.println(btn);
-    if(vipFlag != 1){
+void case1() {
+  if (irrecv.decode(&results) || vipFlag == 1) {
+    //Serial.println(btn);
+    if (vipFlag != 1) {
       btn = btnPressed(results.value);
     }
-    if(mFlag != btn || btn < 10){
+    if (mFlag != btn && btn < 10 && btn > 0) {
       mFlag = btn;
     }
-    else{
-      
+    else {
+
       // right
-      if(btn == 13){
-        mFlag = mFlag+1;
-        if(mFlag == 10)
+      if (btn == 13) {
+        mFlag = mFlag + 1;
+        if (mFlag == 10)
           mFlag = 1;
       }
-       
+
       // left
-      if(btn == 11){
-        mFlag = mFlag-1;
-        if(mFlag == 0)
-          mFlag = 9;  
-      } 
+      if (btn == 11) {
+        mFlag = mFlag - 1;
+        if (mFlag == 0)
+          mFlag = 9;
+      }
 
       // 0, ok when vipFlag is 0
-      if((vipFlag == 0)&&(btn == 16 || btn == 12)){
+      if ((vipFlag == 0) && (btn == 16 || btn == 12)) {
         sFlag = 0;
-      }     
+        mFlag = 0;
+      }
     }
     vipFlag = 0;
     newMFlag();
     irrecv.resume();
-  } 
-} 
+  }
+}
 
 // set the color
-void setColor(int r, int g, int b){
-  Driver.SetColor(r,g,b);
+void setColor(int r, int g, int b) {
+  Driver.SetColor(r, g, b);
 }
 
 
-int btnPressed(int value){
-  switch(value){
-    
+int btnPressed(int value) {
+  switch (value) {
+
     case 0xff6897:
       return 1;
       break;
-      
+
     case 0xff9867:
       return 2;
       break;
-      
+
     case 0xffb04f:
       return 3;
       break;
-      
+
     case 0xff30cf:
       return 4;
       break;
-      
+
     case 0xff18e7:
       return 5;
       break;
-      
+
     case 0xff7a85:
       return 6;
       break;
-      
+
     case 0xff10ef:
       return 7;
       break;
-      
+
     case 0xff38c7:
       return 8;
       break;
-      
+
     case 0xff5aa5:
       return 9;
       break;
-      
+
     case 0xff42bd:
       // *
       return 15;
       break;
 
-    // 0  
+    // 0
     case 0xff4ab5:
       return 16;
       break;
 
-    // #  
+    // #
     case 0xff52ad:
       return 17;
       break;
 
-    // up  
+    // up
     case 0xff629d:
       return 10;
       break;
 
-    // left  
+    // left
     case 0xff22dd:
       return 11;
       break;
 
-    // ok  
+    // ok
     case 0xff02fd:
       return 12;
       break;
 
-    // right  
+    // right
     case 0xffc23d:
       return 13;
       break;
 
-    // down  
+    // down
     case 0xffa857:
       return 14;
       break;
   }
 }
+
 
